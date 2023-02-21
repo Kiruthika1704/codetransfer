2f154990eb2007bee48d62e36c523397d04fb098
Kiruthika1704 <113247750+Kiruthika1704@users.noreply.github.com>
2023-02-21 14:24:47 +0530

    Update ADD 1 TO A GIVEN NUMBER.cpp

--- transcode/data/evaluation/geeks_for_greeks_successful_test_scripts/cpp/ADD 1 TO A GIVEN NUMBER.cpp
+++ transcode/data/evaluation/geeks_for_greeks_successful_test_scripts/cpp/ADD 1 TO A GIVEN NUMBER.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iomanip>
+#include <bits/stdc++.h>
+using namespace std;
+int f_gold ( int x ) {
+  int m = 1;
+  while ( x & m ) {
+    x = x ^ m;
+    m <<= 1;
+  }
+  x = x ^ m;
+  return x;
+}
 
+
+//TOFILL
+
+int main() {
+    int n_success = 0;
+    vector<int> param0 {96,66,67,13,75,78,1,83,27,65};
+    for(int i = 0; i < param0.size(); ++i)
+    {
+        if(f_filled(param0[i]) == f_gold(param0[i]))
+        {
+            n_success+=1;
+        }
+    }
+    cout << "#Results:" << " " << n_success << ", " << param0.size();
+    return 0;
+}
